0904b93a9a7db1b87a5aa9bde0dcd2ed2842335b
Henri <henri@habilestudio.com>
2019-09-18 16:31:49 +0200

    List posts and  up/down voting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-blog';
+  posts = [
+    {
+      title: "Notre top des chats trilingues - le quatrième va vous impressionner",
+      content: "Nam sole orto magnitudine angusti gurgitis sed profundi a transitu arcebantur et dum piscatorios quaerunt lenunculos vel innare temere contextis cratibus parant, effusae legiones, quae hiemabant tunc apud Siden, isdem impetu occurrere veloci. et signis prope ripam locatis ad manus comminus conserendas denseta scutorum conpage semet scientissime praestruebant, ausos quoque aliquos fiducia nandi vel cavatis arborum truncis amnem permeare latenter facillime trucidarunt.",
+      loveIts: 0,
+      createdAt: new Date('September 18, 2018 8:35:00')
+    },
+    {
+      title: "Comment un navet a fait le tour du monde...",
+      content: "Et olim licet otiosae sint tribus pacataeque centuriae et nulla suffragiorum certamina set Pompiliani redierit securitas temporis, per omnes tamen quotquot sunt partes terrarum, ut domina suscipitur et regina et ubique patrum reverenda cum auctoritate canities populique Romani nomen circumspectum et verecundum.",
+      loveIts: -1,
+      createdAt: new Date('December 17, 2019 15:24:00')
+    },
+    {
+      title: "Patrick Balkany déclare : 'Balek, je suis pas vieux, osef !'",
+      content: "Batnae municipium in Anthemusia conditum Macedonum manu priscorum ab Euphrate flumine brevi spatio disparatur, refertum mercatoribus opulentis, ubi annua sollemnitate prope Septembris initium mensis ad nundinas magna promiscuae fortunae convenit multitudo ad commercanda quae Indi mittunt et Seres aliaque plurima vehi terra marique consueta.",
+      loveIts: 2,
+      createdAt: new Date('September 14, 2019 14:10:00')
+    },
+  ]
 }
